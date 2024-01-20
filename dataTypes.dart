719fc618376e2369b1999bacd5639487500949b0
David Richard <elidovrich.coding@gmail.com>
2024-01-20 12:59:43 -0500

    first commit

---
+++ dataTypes.dart
@@ -0,0 +1,21 @@
+void main () {
+  // Strings
+  String firstName = "John";
+  print("String: $firstName");
+
+  // Integers
+  int myNum = 41;
+  print("Integer: $myNum");
+
+  // Doubles
+  double decimalNum = 19.95;
+  print("Double: $decimalNum");
+
+  // Booleans
+  bool myBool = false;
+  print("Boolean: $myBool");
+
+  // Dynamic
+  dynamic fullName = "I can be anything... You will find out who I am during runtime ;)";
+  print("Dynamic: $fullName");
+}
