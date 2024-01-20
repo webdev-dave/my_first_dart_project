9a4f03a6d6bc26d6ebd1e1b713e742e3b053ac70
David Richard <elidovrich.coding@gmail.com>
2024-01-20 14:17:15 -0500

    complete logic basics

---
+++ logic.dart
@@ -0,0 +1,10 @@
+void main() {
+  var num = 3;
+  if (num == 5) {
+    print("The number is 5!");
+  } else if(num == 3){
+    print("The number is 3!");
+  } else {
+    print("The number is neither 3 nor 5!");
+  }
+}
